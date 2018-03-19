9db5ba6eade7f119b447dae0236c62ebc36a68e0
Riki Suzuki <emper96@gmail.com>
2018-03-18 20:59:38 -0400

    Create index.js

---
+++ public/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#top").mouseenter(function(event) {
+    /* When mouse hovers over id = top */
+    $(this).css("background-color", "#ffffff");
+  });
+  $("#top").mouseleave(function(event) {
+    /* Act on the event */
+
+    });("slow")
+    $(this).css("background-color", "#000000");
+  });
+  $("#top").click(function(event) {
+    /* Act on the event */
+
+  });
+    /* Act on the event */
+  });
+});
