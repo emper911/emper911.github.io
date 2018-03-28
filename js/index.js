36dc8e7d26f2ce72668f7d7cb137a46f4a9dadb0
Riki Suzuki <emper96@gmail.com>
2018-03-28 12:51:41 -0400

    Reorganized file system and made changes to main index page.

--- js/index.js
+++ js/index.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
 
   $("#github-link").hover(function(){
-    $("#github-image").attr("src", "images/GitHub-Mark-Light-64px.png");
+    $("#github-image").attr("src", "images/footer/GitHub-Mark-Light-64px.png");
   },function(){
-    $("#github-image").attr("src", "images/GitHub-Mark-64px.png");
+    $("#github-image").attr("src", "images/footer/GitHub-Mark-64px.png");
   });
 
   $("#linkedin-link").hover(function(){
-    $("#linkedin-image").attr("src", "images/In-White-66px-R.png");
+    $("#linkedin-image").attr("src", "images/footer/In-White-66px-R.png");
   },function(){
-    $("#linkedin-image").attr("src", "images/In-Black-66px-R.png");
+    $("#linkedin-image").attr("src", "images/footer/In-Black-66px-R.png");
   });
   $("#resume-link").hover(function(){
-    $("#resume-image").attr("src","images/resumeIconWhite.png");
+    $("#resume-image").attr("src","images/footer/resumeIconWhite.png");
   },function(){
-    $("#resume-image").attr("src","images/resumeIcon.png");
+    $("#resume-image").attr("src","images/footer/resumeIcon.png");
   });
   
 
