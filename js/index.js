4280da3d3c89055009538d7bb696c3921ffdced0
Riki Suzuki <emper96@gmail.com>
2018-03-28 02:17:38 -0400

    Changed resume Icon and reformatted text

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
 
   $("#github-link").hover(function(){
-    $("#github-image").attr("src", "images/GitHub-Mark-Light-64px.png")
+    $("#github-image").attr("src", "images/GitHub-Mark-Light-64px.png");
   },function(){
-    $("#github-image").attr("src", "images/GitHub-Mark-64px.png")
-  }
-  );
+    $("#github-image").attr("src", "images/GitHub-Mark-64px.png");
+  });
+
   $("#linkedin-link").hover(function(){
-    $("#linkedin-image").attr("src", "images/In-White-66px-R.png")
+    $("#linkedin-image").attr("src", "images/In-White-66px-R.png");
   },function(){
-    $("#linkedin-image").attr("src", "images/In-Black-66px-R.png")
-  }
-  );
+    $("#linkedin-image").attr("src", "images/In-Black-66px-R.png");
+  });
+  $("#resume-link").hover(function(){
+    $("#resume-image").attr("src","images/resumeIconWhite.png");
+  },function(){
+    $("#resume-image").attr("src","images/resumeIcon.png");
+  });
+  
 
 });
