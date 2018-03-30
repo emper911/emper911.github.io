3e0f0b0368f3a9733eebab015e074708f8563ca6
Riki Suzuki <emper96@gmail.com>
2018-03-30 15:33:41 -0400

    Reformatted and added two pages. Got rid of the about page, not necessary.

--- js/footer.js
+++ js/footer.js
@@ -1,33 +1,51 @@
 $(document).ready(function() {
     //boolean value isIndex = true or false
     $("#github-link").hover(function(){
-        //if current document is not index then
-        //$("#github-image").attr("src", "../images/footer/GitHub-Mark-Light-64px.png");
-        $("#github-image").attr("src", "images/footer/GitHub-Mark-Light-64px.png");
+        if($("html").attr("id") == "home"){
+            $("#github-image").attr("src", "images/footer/GitHub-Mark-Light-64px.png");
+        }//if current document is not index then
+        else{
+            $("#github-image").attr("src", "../images/footer/GitHub-Mark-Light-64px.png");
+        }
     }, function(){
-        //if current document is not index then
-        //$("#github-image").attr("src", "../images/footer/GitHub-Mark-64px.png");
-        $("#github-image").attr("src", "images/footer/GitHub-Mark-64px.png");
+        if($("html").attr("id") == "home"){
+            $("#github-image").attr("src", "images/footer/GitHub-Mark-64px.png");
+        }//if current document is not index then
+        else{
+            $("#github-image").attr("src", "../images/footer/GitHub-Mark-64px.png");
+        }
     });
 
     $("#linkedin-link").hover(function(){
-        //if current document not index then
-        //$("#linkedin-image").attr("src", "../images/footer/In-White-66px-R.png");
-        $("#linkedin-image").attr("src", "images/footer/In-White-66px-R.png");
+        if($("html").attr("id") == "home"){
+            $("#linkedin-image").attr("src", "images/footer/In-White-66px-R.png");
+        }//if current document is not index then
+        else{
+            $("#linkedin-image").attr("src", "../images/footer/In-White-66px-R.png");
+        } 
      }, function(){
-        //if current document not index then
-        //$("#linkedin-image").attr("src", "../images/footer/In-Black-66px-R.png");
-        $("#linkedin-image").attr("src", "images/footer/In-Black-66px-R.png");
+        if($("html").attr("id") == "home"){
+            $("#linkedin-image").attr("src", "images/footer/In-Black-66px-R.png");
+        }//if current document is not index then
+        else{
+            $("#linkedin-image").attr("src", "../images/footer/In-Black-66px-R.png");
+        } 
     });
 
     $("#resume-link").hover(function(){
-        //if current document not index then
-        //$("#resume-image").attr("src","../images/footer/resumeIconWhite.png");
-        $("#resume-image").attr("src","images/footer/resumeIconWhite.png");
+        if($("html").attr("id") == "home"){
+            $("#resume-image").attr("src","images/footer/resumeIconWhite.png");
+        }//if current document is not index then
+        else{
+            $("#resume-image").attr("src","../images/footer/resumeIconWhite.png");
+        } 
     },function(){
-        //if current document not index then
-        //$("#resume-image").attr("src","../images/footer/resumeIcon.png");
-        $("#resume-image").attr("src","images/footer/resumeIcon.png");
+        if($("html").attr("id") == "home"){
+            $("#resume-image").attr("src","images/footer/resumeIcon.png");
+        }//if current document is not index then
+        else{
+            $("#resume-image").attr("src","../images/footer/resumeIcon.png");
+        } 
     });
   
 
